454b9cc1ba85bd055f9b46c4d3d6f0ed4cd1c55e
MacaroniMutton <mananskher@gmail.com>
2023-11-17 17:27:39 +0530

    Strong Password and made username and email unique

--- DjangoGFG/authentication/models.py
+++ DjangoGFG/authentication/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Player(models.Model):                         #anytime we want to have a table in our database we need to have model for that
     username=models.CharField(max_length=20, unique=True)
-    email=models.EmailField()
+    email=models.EmailField(unique=True)
     password=models.CharField(max_length=20)
     
     
--- DjangoGFG/authentication/static/CSS_JS/script.js
+++ DjangoGFG/authentication/static/CSS_JS/script.js
@@ -8,6 +8,34 @@ signUpButton.addEventListener('click', () =>
 signInButton.addEventListener('click', () =>
     container.classList.remove('right-panel-active'));
 
-// https://Github.com/YasinDehfuli
-// 	 https://Codepen.io/YasinDehfuli
-// Disigned By Nisay
+
+
+    function validatePassword() {
+        // Get the entered password
+        var password = document.getElementById('password').value;
+    
+        // Perform manual password validation
+        if (!isPasswordValid(password)) {
+            // Display an error message
+            alert('Password must be at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, and 1 special symbol.');
+            return false;  // Prevent form submission
+        }
+    
+        return true;  // Allow form submission
+    }
+    
+    function isPasswordValid(password) {
+        // Define your password complexity requirements
+        var lengthRegex = /.{8,}/;
+        var uppercaseRegex = /[A-Z]/;
+        var lowercaseRegex = /[a-z]/;
+        var digitRegex = /\d/;
+        var symbolRegex = /[\W_]/; // Matches any non-alphanumeric character
+    
+        // Check if the password meets all requirements
+        return lengthRegex.test(password) &&
+            uppercaseRegex.test(password) &&
+            lowercaseRegex.test(password) &&
+            digitRegex.test(password) &&
+            symbolRegex.test(password);
+    }
